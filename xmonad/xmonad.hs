1efd10a00aacbbe1c675afa30c262088b8be47ca
Johannes Barre <igel@igels.net>
2015-08-25 09:19:20 +0200

    Replaced xmobar with taffybar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,15 +4,15 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops        (ewmh)
+import System.Taffybar.Hooks.PagerHints (pagerHints)
 
 main = do
-  xmobar <- spawnPipe "xmobar"
-  xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
+  xmobar <- spawnPipe "~/.cabal/bin/taffybar"
+  xmonad $ ewmh $ pagerHints $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , logHook = dynamicLogWithPP $ xmobarPP {ppCurrent = xmobarColor "#b58900" "" . wrap "[" "]", ppUrgent = xmobarColor "#dc322f" "#002b36" . xmobarStrip, ppOutput = hPutStrLn xmobar , ppTitle = xmobarColor "#859900" "" . shorten 70}
     , normalBorderColor = myNormalBorderColor
     , focusedBorderColor = myFocusedBorderColor
     , borderWidth = myBorderWidth
@@ -28,5 +28,4 @@ main = do
 myNormalBorderColor = "#586e75"
 myFocusedBorderColor = "#b58900"
 myBorderWidth = 2
-xmobarCurrentWorkspaceColor = "#b58900"
 myWorkspaces = ["editor","browser","chat","four","five"] ++ map show [6..9]
