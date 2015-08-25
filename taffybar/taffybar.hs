d57f153f72b32011bf9ae7d90431f34083e09f57
Johannes Barre <igel@igels.net>
2015-08-25 09:19:20 +0200

    Replaced xmobar with taffybar

---
+++ taffybar/taffybar.hs
@@ -0,0 +1,46 @@
+import System.Taffybar
+
+import System.Taffybar.Systray
+import System.Taffybar.TaffyPager
+import System.Taffybar.SimpleClock
+import System.Taffybar.FreedesktopNotifications
+import System.Taffybar.Weather
+import System.Taffybar.MPRIS
+import System.Taffybar.Battery
+
+import System.Taffybar.Widgets.PollingBar
+import System.Taffybar.Widgets.PollingGraph
+
+import System.Information.Memory
+import System.Information.CPU
+import System.Information.Battery
+
+memCallback = do
+  mi <- parseMeminfo
+  return [memoryUsedRatio mi]
+
+cpuCallback = do
+  (userLoad, systemLoad, totalLoad) <- cpuLoad
+  return [totalLoad, systemLoad]
+
+main = do
+  let memCfg = defaultGraphConfig { graphDataColors = [(1, 0, 0, 1)]
+                                  , graphLabel = Just "mem"
+                                  }
+      cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1)
+                                                      , (1, 0, 1, 0.5)
+                                                      ]
+                                  , graphLabel = Just "cpu"
+                                  }
+  let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M:%S</span>" 1
+      pager = taffyPagerNew defaultPagerConfig
+      note = notifyAreaNew defaultNotificationConfig
+      wea = weatherNew (defaultWeatherConfig "EDDT") { weatherTemplate = "$stationPlace$ $tempC$°C $humidity$% hum" } 10
+      mpris = mprisNew defaultMPRISConfig
+      mem = pollingGraphNew memCfg 1 memCallback
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      tray = systrayNew
+      battery = batteryBarNew defaultBatteryConfig 120
+  defaultTaffybar defaultTaffybarConfig { startWidgets = [ pager, note ]
+                                        , endWidgets = [ clock, tray, wea, mem, cpu, battery, mpris ]
+                                        }
