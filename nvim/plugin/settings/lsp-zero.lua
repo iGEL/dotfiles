7e789cb120f35c10d516819c5d1d1f9b81deea2d
Johannes Barre <igel@igels.net>
2023-07-24 15:04:08 +0200

    lsp-zero update

--- nvim/plugin/settings/lsp-zero.lua
+++ nvim/plugin/settings/lsp-zero.lua
@@ -1,8 +1,15 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+lsp.configure('clojure-lsp', {
+  on_attach = function(client, bufnr)
+    print('hello clojure-lsp')
+  end
+})
 lsp.nvim_workspace()
 
-lsp.setup()
+lsp.setup({
+    root_dir = require('lspconfig.util').root_pattern({'.git'})
+})
 
 vim.diagnostic.config({ virtual_text = true })
